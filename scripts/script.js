d30cc5dbfd530965acf053fec1f02561cb2d5662
Vailson dos Santos Silva <vailsoft@hotmail.com>
2020-12-12 14:31:44 -0300

    Add files via upload

---
+++ scripts/script.js
@@ -0,0 +1,29 @@
+var body = document.querySelector('body');
+var h1 = document.createElement('h1');
+h1.setAttribute('id', 'cronometro');
+body.appendChild(h1);
+
+let h = 0
+let m = 0
+let s = 0
+let days = 0
+
+setInterval(() => {
+    if(s > 59){
+        s = 0
+        m++
+        if(m > 59){
+            m = 0
+            h++
+            if(h > 23){
+                h = 0
+                days++
+            }
+        }
+    }
+    h1.textContent = `| Days: ${days<10?'0'+ days:days} 
+    | Hours: ${h < 10?'0'+ h:h} 
+    | Minutes: ${m < 10?'0'+ m:m} 
+    | Secounds: ${s < 10?'0'+ s:s} |`
+    s++
+}, 1000);
